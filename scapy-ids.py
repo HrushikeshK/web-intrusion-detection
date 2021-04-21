211bafd9f36534de3066959d1f95ae282c114b38
hrushikeshk <hru123.kakade@gmail.com>
2021-04-21 18:57:22 +0530

    Redo the working using scapy

--- scapy-ids.py
+++ scapy-ids.py
@@ -41,6 +41,64 @@ def process_packet(packet):
             # then show raw
             print(f"\n{RED}[*] Some useful Raw data: {packet[Raw].load}{RESET}")
 
+        # Check Vulnerabilties
+        check_vulns(url, ip)
+
+def check_vulns(query, ip):
+    if detect_sqli(query):
+        print(f"\n{RED}SQLi detected:{RESET} {query} {ip}")
+    if detect_xss(query):
+        print(f"\n{RED}XSS detected:{RESET} {query} {ip}")
+
+def detect_sqli(query):
+    #Clear Text SQL injection test, will create false positives. 
+    regex=re.compile('drop|delete|truncate|update|insert|select|declare|union|create|concat', re.IGNORECASE)
+    if regex.search(query):
+        return True
+
+    #look for single quote, = and --
+    regex=re.compile('((\%3D)|(=))[^\n]*((\%27)|(\')|(\-\-)|(\%3B)|(;))|\w*((\%27)|(\'))((\%6F)|o|(\%4F))((\%72)|r|(\%52))', re.IGNORECASE)
+    if regex.search(query):
+        return True
+
+    #look for MSExec
+    regex=re.compile('exec(\s|\+)+(s|x)p\w+', re.IGNORECASE)
+    if regex.search(query):
+        return True
+
+    # hex equivalent for single quote, zero or more alphanumeric or underscore characters
+    regex=re.compile('/\w*((\%27)|(\'))((\%6F)|o|(\%4F))((\%72)|r|(\%52))/ix', re.IGNORECASE)
+    if regex.search(query):
+        return True
+
+    return False
+
+
+def detect_xss(query):
+    # "script" and "on" alerts
+    regex = re.compile('(\b)(on\S+)(\s*)=|javascript|(<\s*)(\/*)script', re.IGNORECASE)
+    if regex.search(query):
+        return True
+    
+    # Simple XSS attacks
+    regex = re.compile('((\%3C)|<)((\%2F)|\/)*[a-z0-9\%]+((\%3E)|>)')
+    if regex.search(query):
+        return True
+
+
+    # XSS for "<img src" attack
+    regex = re.compile('((\%3C)|<)((\%69)|i|(\%49))((\%6D)|m|(\%4D))((\%67)|g|(\%47))[^\n]+((\%3E)|>)')
+    if regex.search(query):
+        return True
+
+    # XSS with anything with "<" or ">". Paranoid XSS detection
+    regex = re.compile('((\%3C)|<)[^\n]+((\%3E)|>)')
+    if regex.search(query):
+        return True
+
+    return False
+
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="HTTP Packet Sniffer, this is useful when you're a man in the middle." \
